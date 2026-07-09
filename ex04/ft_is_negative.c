83797669687f9b480ef5baf3e72dcf9165ac37d7
Alison Camelo <acamelo@c1r10s2.42madrid.com>
2026-07-09 13:28:21 +0200

    Mejorando sintaxis

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_negative.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acamelo <acamelo@student.42madrid.com      +#+  +:+       +#+        */
+/*   By: acamelo <acamelo@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 19:38:54 by acamelo           #+#    #+#             */
-/*   Updated: 2026/02/02 19:44:17 by acamelo          ###   ########.fr       */
+/*   Updated: 2026/07/09 13:28:00 by acamelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ void	ft_is_negative(int n)
 	u = 'N';
 	p = 'P';
 	if (n < 0)
-	{
 		write(1, &u, 1);
-	}
 	else
 	{
 		write(1, &p, 1);
